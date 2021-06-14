b4ff4a7c2040db1082a0605f7f912bfd9cdbcf4c
asab07 <asabir0777@gmail.com>
2021-06-14 18:58:04 -0400

    added feature js file

---
+++ feature.js
@@ -0,0 +1,6 @@
+const heading = document.querySelector("h1");
+const paragraph = document.querySelector('p')
+
+const sayHello = (name) => `Hello ${name}!`;
+
+heading.textContent = sayHello("Adeel");
