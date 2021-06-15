451ee3edfce8e86ee32c8f71ffb00a0b4666dc29
asab07 <asabir0777@gmail.com>
2021-06-14 20:36:22 -0400

    minor change

--- feature.js
+++ feature.js
@@ -1,8 +1,8 @@
 const heading = document.querySelector("h1");
-const paragraph = document.querySelector('p')
+const paragraph = document.querySelector("p");
 
 const sayHello = (name) => `Hello ${name}!`;
 
 heading.textContent = sayHello("Adeel");
-console.log('Hello world');
-
+console.log("Hello world");
+console.log("How are you");
